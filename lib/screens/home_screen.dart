f9616a29b10a487b133ae6c160406be8529e01e3
Nathaniel Green <me@nathaniel.green>
2021-07-23 20:39:37 +0100

    Add future builder to homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:lifx_http_api/lifx_http_api.dart' show Bulb, Client;
 import '../widgets/global/top_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final client = Client("");
+  late Future<Iterable<Bulb>> futureLights;
+
+  @override
+  void initState() {
+    super.initState();
+    futureLights = client.listLights();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopBar(),
-      body: const Center(child: Text('Home')),
+      body: Center(
+        child: FutureBuilder<Iterable<Bulb>>(
+          future: futureLights,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.toString());
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
     );
   }
 }
