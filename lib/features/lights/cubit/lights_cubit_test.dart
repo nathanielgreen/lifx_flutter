a764863ff492c68e4884c6822b1bb692c163dfc0
Nathaniel Green <me@nathaniel.green>
2021-08-23 17:11:37 +0100

    Add mock bulb to test

--- lib/features/lights/cubit/lights_cubit_test.dart
+++ lib/features/lights/cubit/lights_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:lifx_http_api/lifx_http_api.dart';
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:bloc_test/bloc_test.dart';
@@ -8,6 +9,17 @@ import './lights_cubit.dart';
 
 class MockLightsRepository extends Mock implements LightsRepository {}
 
+final mockBulb = Bulb(
+  uuid: 'uuid',
+  id: 'id',
+  label: 'Mock Bulb',
+  connected: true,
+  group: Group(id: 'group-id', name: 'Mock Group'),
+  brightness: 100,
+  power: 'on',
+  color: LifxColor(hue: 1.0, saturation: 1.0, kelvin: 2000),
+);
+
 void main() {
   group('LightsCubit', () {
     late LightsCubit lightsCubit;
@@ -15,7 +27,7 @@ void main() {
     setUp(() {
       final mockLightsRepository = MockLightsRepository();
       when(() => mockLightsRepository.getBulbs())
-          .thenAnswer((_) => Future.value([]));
+          .thenAnswer((_) => Future.value([mockBulb]));
 
       lightsCubit = LightsCubit(mockLightsRepository);
     });
@@ -26,7 +38,7 @@ void main() {
 
     test('lights loaded on getLights()', () async {
       await lightsCubit.getLights();
-      expect(lightsCubit.state, const LightsLoaded([]));
+      expect(lightsCubit.state, LightsLoaded([mockBulb]));
     });
   });
 }
